8730fc01e65de519dd2bda7dfc5be40c717bf8eb
jonaNewberry <jona_nn@hotmail.com>
2025-07-30 14:16:47 -0300

    index.html

--- script.js
+++ script.js
@@ -66,4 +66,16 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+  document.getElementById('toggle-ados').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    const details = document.getElementById('ados-details');
+    if (details.style.display === 'none' || details.style.display === '') {
+      details.style.display = 'inline'; // Muestra el contenido
+      this.textContent = '... Leer menos'; // Cambia el texto del enlace
+    } else {
+      details.style.display = 'none'; // Oculta el contenido
+      this.textContent = '... Leer más'; // Cambia el texto del enlace
+    }
+  });
+
 //whatsapp  //
